f3c23f266c5fe782a4a0b12d1692536142249718
Rao Mobeen-ul-haq <raomobeen@hotmail.co.uk>
2022-06-20 17:16:21 -0400

    removes time functionality

--- file-writer.py
+++ file-writer.py
@@ -1,6 +1,5 @@
 import ftxdata
 from datetime import datetime
-import asyncio
 import csv
 
 class FileWriter:
@@ -9,29 +8,19 @@ class FileWriter:
         self.filename = filename
 
     # file appending function
-    async def fileAppend(self, data):
+    def fileAppend(self, data):
         #data = test2.Data()
 
         with open(self.filename, 'a', newline="") as f:
             writer = csv.writer(f)
-            while True:
-                now = datetime.now()
-                await asyncio.sleep(60) # data needs to be plotted and analysed per 6o seconds
-                writer.writerow([now, data.get_price(), data.get_oi()])
+            now = datetime.now()
+            writer.writerow([now, data.get_price(), data.get_oi()])
 
 if __name__ == "__main__":
 
-    path = ''
+    path = 'price-oi.csv'
     fwriter = FileWriter(path)
     data = ftxdata.Data()
+    fwriter.fileAppend(data)
 
-    loop = asyncio.get_event_loop()
-
-    try:
-        asyncio.ensure_future(fwriter.fileAppend(data))
-        loop.run_forever()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        print("Closing Loop")
-        loop.close()
+    
